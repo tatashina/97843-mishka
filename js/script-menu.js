8801a50bdf2a5157c1ae3939ee05464f2ffba7dc
Ann <anna.tatashina@gmail.com>
2017-11-09 19:35:53 +0300

    fixes

--- js/script-menu.js
+++ js/script-menu.js
@@ -1,14 +1,20 @@
 var navMain = document.querySelector('.main-header__nav');
 var navToggle = document.querySelector('.main-header__nav-burger');
+var contMenu = document.querySelector('.main-header__container');
+
+
+navMain.classList.remove('main-header__nav--nojs');
+contMenu.classList.remove('container--menu-nojs');
+navMain.classList.add('main-header__nav--closed');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header--closed')) {
-    navMain.classList.remove('main-header--closed');
-    navMain.classList.add('main-header--opened');
+  if (navMain.classList.contains('main-header__nav--closed')) {
+    navMain.classList.remove('main-header__nav--closed');
+    navMain.classList.add('main-header__nav--opened');
   }
 
   else {
-    navMain.classList.add('main-header--closed');
-    navMain.classList.remove('main-header--opened');
+    navMain.classList.add('main-header__nav--closed');
+    navMain.classList.remove('main-header__nav--opened');
   }
 });
